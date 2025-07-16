cbcbf97b978f71c12bc5c8e106acfa4b4d8e8781
Sourajit Datta <dattasourajit04@gmail.com>
2025-07-16 11:27:00 +0530

    first commit

--- RegistrationMVCApp/obj/Debug/net8.0/RegistrationMVCApp.AssemblyInfo.cs
+++ RegistrationMVCApp/obj/Debug/net8.0/RegistrationMVCApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RegistrationMVCApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95dcf466408cd35d2f6ce3067bc992a26304252e")]
 [assembly: System.Reflection.AssemblyProductAttribute("RegistrationMVCApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RegistrationMVCApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
