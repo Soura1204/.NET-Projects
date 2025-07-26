e6d294a60d2faa2f3539dc8e5b895f506791aeb5
Sourajit Datta <91087993+Soura1204@users.noreply.github.com>
2025-07-26 13:30:03 +0530

    Delete RegistrationMVCApp directory

--- RegistrationMVCApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace RegistrationMVCApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
